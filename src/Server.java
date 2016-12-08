06a28549e6e767dd331fc21b2dd9cdba6bd2d08b
Jeroen Smienk <jeroensmienk@icloud.com>
2016-12-08 13:39:07 +0100

    Server klasse gemaakt

---
+++ src/Server.java
@@ -0,0 +1,14 @@
+/**
+ * Author: Jeroen
+ * Date created: 08-12-16
+ */
+public class Server {
+
+    public static void main(String[] args) {
+        new Server().run();
+    }
+
+    private void run() {
+
+    }
+}
